47f5aabb1ac41232b178175b0a5d4647fd7b4073
LeoMbm <leonidas.j.mbm@gmail.com>
2022-06-15 20:24:06 +0200

    start: create lobby and add user in lobby

--- Routes/dataRoutes.js
+++ Routes/dataRoutes.js
@@ -6,10 +6,10 @@ const router = Router();
 router.get("/api/users", dataControllers.allUsers_get);
 router.get("/api/users/:id", dataControllers.userById_get);
 router.delete("/api/users/:id", dataControllers.userById_delete);
-
+// CREATE AND GET LOBBY
 router.get("/api/lobby", dataControllers.allLobby_get);
 router.post("/api/lobby", dataControllers.allLobby_post);
-
+// ADD AND GET USER IN LOBBY
 router.get("/api/lobby/users", dataControllers.userLobby_get);
 router.post("/api/lobby/users", dataControllers.userLobby_post);
 
