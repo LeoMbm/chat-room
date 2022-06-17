1323a927af1577dbcc87d9fad08efc8e8e23e0ae
LeoMbm <leonidas.j.mbm@gmail.com>
2022-06-17 18:06:45 +0200

    fix: heroku redirect

--- Routes/viewsRoute.js
+++ Routes/viewsRoute.js
@@ -5,6 +5,5 @@ const router = Router();
 
 router.get("/", viewsController.index_get);
 router.get("/about", viewsController.about_get);
-router.get("/welcome", viewsController.welcome_get);
 
 module.exports = router;
